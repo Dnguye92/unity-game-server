5163f817120f059fd0b4a86d9291589129c1f6d5
dylan <nguyendylan92@gmail.com>
2020-08-07 03:33:26 -0700

    Add project files.

---
+++ unity-game-server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace unity_game_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Unity Game Server";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
