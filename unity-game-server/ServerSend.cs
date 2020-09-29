d5be0510d5e604d1081e0a8677e0d5a958356abe
dylan <nguyendylan92@gmail.com>
2020-09-29 02:26:53 -0700

    add UDP support

--- unity-game-server/ServerSend.cs
+++ unity-game-server/ServerSend.cs
@@ -13,6 +13,12 @@ namespace unity_game_server
             Server.clients[_toClient].tcp.SendData(_packet);
         }
 
+        private static void SendUDPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].udp.SendData(_packet);
+        }
+
         private static void SendTCPDataToAll(Packet _packet)
         {
             _packet.WriteLength();
@@ -34,6 +40,28 @@ namespace unity_game_server
             }
         }
 
+        private static void SendUDPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i < Server.MaxPlayers; i++)
+            {
+                Server.clients[i].udp.SendData(_packet);
+            }
+        }
+
+        private static void SendUDPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Server.clients[i].udp.SendData(_packet);
+                }
+            }
+        }
+
+
         public static void Welcome(int _toClient, string _msg)
         {
             using (Packet _packet = new Packet((int)ServerPackets.welcome))
@@ -42,6 +70,16 @@ namespace unity_game_server
                 _packet.Write(_toClient);
 
                 SendTCPData(_toClient, _packet);
+            }
+        }
+
+        public static void UDPTest(int _toClient)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.udpTest))
+            {
+                _packet.Write("A test packet for UDP.");
+
+                SendUDPData(_toClient, _packet);
             }
         }
     }
